4af60fb3aeb8ea844ad51bb2eb04019d11f48adc
yVexy <danifilipe221205@gmail.com>
2023-05-31 02:17:03 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void showValorantInfo() {
+	char server[50];
+	char username[50];
+
+	printf("Type the server you play on (example, BR, NA, EU): ");
+	scanf("%s", servidor);
+
+	
+}
+
+int main() {
+	int option;
+
+return 0;	
+} 
